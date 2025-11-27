edaa655a17fbdbd5ad5d82819d624f8995b4f2e3
Andrew <azhan8051@gmail.com>
2025-11-27 18:02:39 -0500

    Update borrowing_routes.py

--- routes/borrowing_routes.py
+++ routes/borrowing_routes.py
@@ -3,7 +3,7 @@ Borrowing Routes - Book borrowing and returning endpoints
 """
 
 from flask import Blueprint, render_template, request, redirect, url_for, flash
-from services.library_service import borrow_book_by_patron, return_book_by_patron
+from library_service import borrow_book_by_patron, return_book_by_patron
 
 borrowing_bp = Blueprint('borrowing', __name__)
 
@@ -49,4 +49,5 @@ def return_book():
     
     flash(message, 'success' if success else 'error')
     return render_template('return_book.html')
+
 
