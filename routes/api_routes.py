9b45e591e054225a811c6555beeb777c7284e010
Andrew <azhan8051@gmail.com>
2025-11-27 18:02:26 -0500

    Update api_routes.py

--- routes/api_routes.py
+++ routes/api_routes.py
@@ -3,7 +3,7 @@ API Routes - JSON API endpoints
 """
 
 from flask import Blueprint, jsonify, request
-from services.library_service import calculate_late_fee_for_book, search_books_in_catalog
+from library_service import calculate_late_fee_for_book, search_books_in_catalog
 
 api_bp = Blueprint('api', __name__, url_prefix='/api')
 
@@ -37,4 +37,5 @@ def search_books_api():
         'results': books,
         'count': len(books)
     })
+
 
